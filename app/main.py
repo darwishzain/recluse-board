8b760497827a17ef9482bba4dc77dd456ee2932d
Darwish Mat Zain <darwish@darwishzain.com>
2022-08-24 19:15:10 +0800

    24082022 1913
    Optimize code
    - open csv file
    - run command (ump-link)
    - open link (ump-link)

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 #import tkinter as tk #! linux : Tkinter #! win10 : tkinter
+from mimetypes import init
 from tkinter import *
 from tkinter import filedialog
 import os,sys
 from pygame import mixer
 import time
 import csv
+
 ### TODO Fix clashing audio
 ### TODO Timetable with choice to show classes only [not important]
 ### TODO Pomodoro timer -
-global light_color,dark_color
 light_color = "#F0F0F0"
 dark_color = "#000000"
 
@@ -18,6 +19,10 @@ appOs = os.name
 appDev = 'Darwish Zain'
 appVer = '0.0.1 - reborn'
 
+showDaily = True
+showMedia = True
+showClock = True
+
 def appInfo():
     print("Running "+appName+" on "+appPlatform)
     time.sleep(1)
@@ -26,63 +31,77 @@ def appInfo():
     print("Version "+appVer)
     time.sleep(1)
 
-appInfo()
+#appInfo()
+def command(cmd):
+    os.system(str(cmd))
+
+def openlink(link):
+    if os.name == 'posix':
+        link='xdg-open '+link
+    elif os.name == 'nt':
+        link = 'explorer '+link
+    os.system(str(link))
+
+def opencsv(thefile):
+    filedata = []
+    if thefile:
+        with open(thefile,'r') as o:
+            reader = csv.reader(o)
+            for row in reader:
+                filedata.append(row)
+            return filedata
 
 root = Tk()
 root.title(appName+" v"+appVer+" on "+appPlatform)
 #root.geometry("1350x400")
 
-
-#? frame where the button is in
+#? frame where the everything is in
 mainFrame = Frame(root, bg='#F0F0F0', height=300, width=500)
 mainFrame.grid(row=0, column=0)
+
 titleFrame = Frame(mainFrame, bg='#FFF')
 titleFrame.grid(row=0, column=4, columnspan=5)
 clockFrame = Frame(mainFrame)
 clockFrame.grid(row=1, column=0)
 tableFrame = Frame(mainFrame)
-tableFrame.grid(row=2, column=0, columnspan=5)
+tableFrame.grid(row=2, column=0, columnspan=5)#0,1,2,3,4
+audioFrame = Frame(mainFrame, bg='#FFF', width="500", height="20")
+audioFrame.grid(row=2, column=5)
 cTFrame = Frame(mainFrame)
 cTFrame.grid(row=3, column=0, columnspan=5)
-mediaFrame = Frame(mainFrame, bg='#FFF', width="500", height="20")
-mediaFrame.grid(row=5, column=0, columnspan=3)
+
 #root.attributes('-zoomed', True)
 #root.state('zoomed')
 
+link = opencsv('csv/link.csv')
+print(len(link))
+
 ######! START TABLE !######
 def dailyPlanner():
-    global tdata
-    tdata = []
-    dailyfile = './csv/daily.csv'
-    if dailyfile:
-        with open(dailyfile,'r') as tcsv:
-            treader = csv.reader(tcsv)
-            for row in treader:
-                tdata.append(row)
-        for yaxis in range(8):
-            for xaxis in range(25):
-                day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-                #today = time.strftime("%A") #? return day
-                #now = time.strftime("%H") #? sreturn hour
-                if(yaxis==0):
-                    if(xaxis!=0):
-                        #? time of the day
-                        tableText = "{0:0=2d}".format(xaxis-1)+"00"
-                    else:
-                        tableText = "Day\Time"
+    dailydata = opencsv('csv/daily.csv')
+    for yaxis in range(8):
+        for xaxis in range(25):
+            day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+            #today = time.strftime("%A") #? return day
+            #now = time.strftime("%H") #? sreturn hour
+            if(yaxis==0):
+                if(xaxis!=0):
+                    #? time of the day
+                    tableText = "{0:0=2d}".format(xaxis-1)+"00"
                 else:
-                    if(xaxis==0):
-                        #if(today == day[yaxis-1]):
-                        #    tablebg = "#00FF00"
-                        tableText = day[yaxis-1]
-                    else:
-                        #if(today == day[yaxis-1] and now == "{0:0=2d}".format(xaxis)):
-                        #    tablebg = "#00FF00"
-                        #    tablefg = "#0F0F0F"
-                        tableText = tdata[yaxis-1][xaxis-1]
-                Label(tableFrame, justify="left", bg=dark_color, fg=light_color, text=tableText).grid(row=yaxis, column=xaxis, ipady=5, sticky='nesw')
+                    tableText = "Day\Time"
+            else:
+                if(xaxis==0):
+                    #if(today == day[yaxis-1]):
+                    #    tablebg = "#00FF00"
+                    tableText = day[yaxis-1]
+                else:
+                    #if(today == day[yaxis-1] and now == "{0:0=2d}".format(xaxis)):
+                    #    tablebg = "#00FF00"
+                    #    tablefg = "#0F0F0F"
+                    tableText = dailydata[yaxis-1][xaxis-1]
+            Label(tableFrame, justify="left", bg=light_color, fg=dark_color, text=tableText).grid(row=yaxis, column=xaxis, ipady=5, sticky='nesw')
 
-dailyPlanner()
 ######! END TABLE !######
 
 ######! START CLOCK !######
@@ -106,7 +125,6 @@ def initClock():
     mixer.init()
 
     updateClock()
-
 
 def countdown(t):
     global timer,isRing
@@ -150,56 +168,76 @@ def updateClock():
 ######! END CLOCK !######
 #*--------------------*#
 ######! START MUSIC PLAYER !######
-def mediaPlayer():
-    global songLabel
-    loadBtn = Button(mediaFrame, text="Load", command=lambda:load())
+isAudio = False
+audio = mixer
+audio.init()
+audioFile = ""
+audioList = []
+def audioPlayer():
+    global audioLabel
+    loadBtn = Button(audioFrame, text="Load", command=lambda:load())
     loadBtn.grid(row=0, column=0, columnspan=5)
 
-    prevBtn = Button(mediaFrame, text="Prev")
+    prevBtn = Button(audioFrame, text="Prev")
     prevBtn.grid(row=2, column=0)
-    stopBtn = Button(mediaFrame, text="Stop", command=lambda:stop())
+    stopBtn = Button(audioFrame, text="Stop", command=lambda:stop())
     stopBtn.grid(row=2, column=1)
-    playBtn = Button(mediaFrame, text="Play", command=lambda:play())
+    playBtn = Button(audioFrame, text="Play", command=lambda:play())
     playBtn.grid(row=2, column=2)
-    pauseBtn = Button(mediaFrame, text="Pause", command=lambda:pause())
+    pauseBtn = Button(audioFrame, text="Pause", command=lambda:pause())
     pauseBtn.grid(row=2, column=3)
-    nextBtn = Button(mediaFrame, text="Next")
+    nextBtn = Button(audioFrame, text="Next")
     nextBtn.grid(row=2, column=4)
-    songLabel = Label(mediaFrame, text="Audio Title")#, relief='flat'
-    songLabel.grid(row=3, column=0, columnspan=5, sticky='nesw')
+
+    audioLabel = Label(audioFrame, text="Audio Title",width="10")#, relief='flat'
+    audioLabel.grid(row=3, column=0, columnspan=5, sticky='nesw')
+    listFrame = Frame(audioFrame)
+    listFrame.grid(row=4,column=0, columnspan=5)
+    listLabel = Label(listFrame, text="What",width=10,height=20)
+    listLabel.grid(row=5, column=0, columnspan=5)
+
 
 
 def load():
-    global musicFile
-    musicFile = filedialog.askopenfilename()
-    songLabel.config(text=musicFile)
-    print("Loading....."+musicFile)
+    global audioFile
+    audioFile = filedialog.askopenfilename()
+    audioLabel.config(text=audioFile)
+    audioList.append(audioFile)
+    print(audioList)
+    print("Loading....."+audioFile)
+
 def play():
-    global playingState
-    if musicFile:
-        musicSound = mixer
-        musicSound.init()
-        musicSound.music.load(musicFile)
-        musicSound.music.play(loops=-1)
+    global isAudio
+    if audioFile:
+        audio.music.load(audioFile)
+        audio.music.play(loops=-1)
+        isAudio = True
+    else:
+        filenotload()
 def pause():
-    if not playingState:
+    global isAudio
+    if not isAudio:
         mixer.music.pause()
-        playingState=True
+        isAudio=True
     else:
         mixer.music.unpause()
-        playingState = False
+        isAudio = False
 def stop():
     mixer.music.stop()
+
+def filenotload():
+    audioLabel.config(text="File Not Loaded")
+
 ######! END MUSIC PLAYER !######
 
+if showDaily:dailyPlanner()
+if showMedia:audioPlayer()
+if showClock:initClock()
 
-initClock()
-mediaPlayer()
-
-#root.iconbitmap('./image/UMP-link.ico')
+root.iconbitmap('./icon.ico')
 root.eval('tk::PlaceWindow . center')#? Application positioned at center of screen
 root.mainloop()
-#darwish Music Player
-#darwish Daily Time Table
-#darwish Pomodoro
-#darwish Clock/Date
+#:] Music Player
+#:] Daily Time Table
+#:] Pomodoro
+#:] Clock/Date
