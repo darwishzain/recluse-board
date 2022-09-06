865df155ecffa6e2a75454426e3353df8fbc5da1
Darwish Mat Zain <darwish@darwishzain.com>
2022-09-07 01:03:09 +0800

    07092022 0103

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 #import tkinter as tk #! linux : Tkinter #! win10 : tkinter
-from mimetypes import init
 from tkinter import *
 from tkinter import filedialog
 import os,sys
@@ -7,9 +6,9 @@ from pygame import mixer
 import time
 import csv
 
-### TODO Fix clashing audio
-### TODO Timetable with choice to show classes only [not important]
-### TODO Pomodoro timer -
+### todo Fix clashing audio
+### todo Timetable with choice to show classes only [not important]
+### todo Pomodoro timer -
 light_color = "#F0F0F0"
 dark_color = "#000000"
 
@@ -55,7 +54,7 @@ root = Tk()
 root.title(appName+" v"+appVer+" on "+appPlatform)
 #root.geometry("1350x400")
 
-#? frame where the everything is in
+#? frame where everything is in
 mainFrame = Frame(root, bg='#F0F0F0', height=300, width=500)
 mainFrame.grid(row=0, column=0)
 
@@ -67,14 +66,18 @@ tableFrame = Frame(mainFrame)
 tableFrame.grid(row=2, column=0, columnspan=5)#0,1,2,3,4
 audioFrame = Frame(mainFrame, bg='#FFF', width="500", height="20")
 audioFrame.grid(row=2, column=5)
-cTFrame = Frame(mainFrame)
-cTFrame.grid(row=3, column=0, columnspan=5)
+pomoFrame = Frame(mainFrame)
+pomoFrame.grid(row=3, column=0, columnspan=5)
 
 #root.attributes('-zoomed', True)
 #root.state('zoomed')
 
 link = opencsv('csv/link.csv')
-print(len(link))
+
+for i in link:
+    print(i[0])
+    print(i[1])
+
 
 ######! START TABLE !######
 def dailyPlanner():
@@ -105,42 +108,18 @@ def dailyPlanner():
 ######! END TABLE !######
 
 ######! START CLOCK !######
-isRing = FALSE
 def initClock():
-    global clockDisplay, dateDisplay, timerDisplay
-    global timer, isRing
-    timer=0
+    global clockDisplay, dateDisplay
     clockDisplay = Label(clockFrame, font=('Arial',30), text="HH:MM::SS")
     clockDisplay.grid(row=0, column=0, columnspan=2)
     dateDisplay = Label(clockFrame, font=('Arial',30),text="DD/MM/xaxisY")
     dateDisplay.grid(row=0, column=3, ipadx=10, columnspan=2)
-    timerDisplay = Label(clockFrame, font=('Arial',20), text=0)
-    timerDisplay.grid(row=0, column=6)
-    pomoThirty = Button(clockFrame, text="+30s", command=lambda:countdown(3))#! testing 3.lama sgt nk tunggu 30
-    pomoThirty.grid(row=0, column=7)
-    pomoInput = Text(clockFrame, height="1", width="10")
-    pomoInput.grid(row=0, column=9)
-    snoozeBtn = Button(clockFrame, text="Snooze", command=lambda:snooze())
-    snoozeBtn.grid(row=0, column=8)
-    mixer.init()
 
     updateClock()
-
-def countdown(t):
-    global timer,isRing
-    isRing = TRUE
-    timer = timer + t
-
-def snooze():
-    global isRing
-    isRing = FALSE
 
 def updateClock():
     global cH24, cH12, cM, cS #? clock variable [Hour24 Hour12 Minute Second]
     global dD, dM, dY #? Date variable [Day Month Year]
-    global timer #? timer variable
-    global playingState
-    pomoSound = mixer
 
     cH24 = time.strftime("%H")
     cH12 = time.strftime("%I")
@@ -153,15 +132,6 @@ def updateClock():
     clockDisplay.config(text=clock)
     date = dD+"/"+dM+"/"+dY
     dateDisplay.config(text=date)
-    if timer>0:
-        timer-=1
-        timerDisplay.config(text=timer)
-    elif isRing:
-        pomoSound.init()
-        sound = "./alarm.mp3"
-        pomoSound.Channel(0).play(sound,loops=-1)
-    #elif isRing==FALSE:
-    #    pomoSound.music.pause()#! issue: music player also paused
 
     root.after(1000,updateClock)
 
@@ -173,16 +143,18 @@ audio = mixer
 audio.init()
 audioFile = ""
 audioList = []
+isAudio = False
 def audioPlayer():
-    global audioLabel
+    #TODO previous, next
+    global audioLabel,listLabel,listFrame
     loadBtn = Button(audioFrame, text="Load", command=lambda:load())
-    loadBtn.grid(row=0, column=0, columnspan=5)
+    loadBtn.grid(row=1, column=0, columnspan=5)
 
     prevBtn = Button(audioFrame, text="Prev")
     prevBtn.grid(row=2, column=0)
     stopBtn = Button(audioFrame, text="Stop", command=lambda:stop())
     stopBtn.grid(row=2, column=1)
-    playBtn = Button(audioFrame, text="Play", command=lambda:play())
+    playBtn = Button(audioFrame, text="Play", command=lambda:play(len(audioList)-1))
     playBtn.grid(row=2, column=2)
     pauseBtn = Button(audioFrame, text="Pause", command=lambda:pause())
     pauseBtn.grid(row=2, column=3)
@@ -190,38 +162,46 @@ def audioPlayer():
     nextBtn.grid(row=2, column=4)
 
     audioLabel = Label(audioFrame, text="Audio Title",width="10")#, relief='flat'
-    audioLabel.grid(row=3, column=0, columnspan=5, sticky='nesw')
-    listFrame = Frame(audioFrame)
+    audioLabel.grid(row=0, column=0, columnspan=5, sticky='nesw')
+    listFrame = Frame(audioFrame, width=10)
     listFrame.grid(row=4,column=0, columnspan=5)
-    listLabel = Label(listFrame, text="What",width=10,height=20)
-    listLabel.grid(row=5, column=0, columnspan=5)
+    # listLabel = Label(listFrame, text="Playlist",height=10)
+    # listLabel.grid(row=1, column=0, columnspan=5)
 
 
 
 def load():
     global audioFile
+    i = len(audioList)
     audioFile = filedialog.askopenfilename()
-    audioLabel.config(text=audioFile)
+    audioLabel.config(text=os.path.basename(audioFile))
     audioList.append(audioFile)
-    print(audioList)
-    print("Loading....."+audioFile)
+    # Button(listFrame, text=audioList[i].split("/",2),command=lambda:play(i)).grid(row=i, column=0, columnspan=5)
+    Button(listFrame, text=os.path.basename(audioList[i]),command=lambda:play(i)).grid(row=i, column=0, columnspan=5)
+    # print(audioList)
+    # print("Loading....."+audioFile)
 
-def play():
-    global isAudio
+def play(i):
+    audioFile = audioList[i]
     if audioFile:
         audio.music.load(audioFile)
         audio.music.play(loops=-1)
         isAudio = True
     else:
         filenotload()
+
+# *for local variable referenced before assignment,
+# *set variable to global
+# *function asssumes variable is local
+
 def pause():
-    global isAudio
-    if not isAudio:
-        mixer.music.pause()
-        isAudio=True
-    else:
+    global isAudio#:] solution for referenced before variable
+    if isAudio:
         mixer.music.unpause()
-        isAudio = False
+    else:
+        mixer.music.pause()
+    isAudio = not isAudio
+
 def stop():
     mixer.music.stop()
 
@@ -230,13 +210,73 @@ def filenotload():
 
 ######! END MUSIC PLAYER !######
 
+######! START POMODORO######
+timer = 0
+def pomo():
+    pomoStart = Button(pomoFrame, text="Set", command=lambda:setPomo(10))#! testing 3.lama sgt nk tunggu 30
+    pomoStart.grid(row=0, column=0)
+    global pomoCount
+    pomoCount = Label(pomoFrame, text=timer)
+    pomoCount.grid(row=0, column=1)
+pomo()
+def setPomo(t):
+    global timer
+    timer = timer + t
+    countdown()
+
+def countdown():
+    global timer
+    timer -= 1
+    pomoCount.config(text=timer)
+    root.after(1000,countdown)
+
+######! END POMODORO######
 if showDaily:dailyPlanner()
 if showMedia:audioPlayer()
 if showClock:initClock()
 
+def refresh():
+    root.after(1000,refresh)
+refresh()
+
+
 root.iconbitmap('./icon.ico')
 root.eval('tk::PlaceWindow . center')#? Application positioned at center of screen
 root.mainloop()
+# global timer #? timer variable
+# global playingState
+# pomoSound = mixer
+# if timer>0:
+#     timer-=1
+#     timerDisplay.config(text=timer)
+# elif isRing:
+#     pomoSound.init()
+#     sound = "./alarm.mp3"
+#     pomoSound.Channel(0).play(sound,loops=-1)
+# #elif isRing==FALSE:
+# #    pomoSound.music.pause()#! issue: music player also paused
+
+# global timer, isRing
+# timer=0
+
+# pomoInput = Text(clockFrame, height="1", width="10")
+# pomoInput.grid(row=0, column=9)
+# snoozeBtn = Button(clockFrame, text="Snooze", command=lambda:snooze())
+# snoozeBtn.grid(row=0, column=8)
+# mixer.init()
+# isRing = FALSE
+
+# def countdown(t):
+#     global timer,isRing
+#     isRing = TRUE
+#     timer = timer + t
+
+# def snooze():
+#     global isRing
+#     isRing = FALSE
+# timerDisplay = Label(clockFrame, font=('Arial',20), text=0)
+# timerDisplay.grid(row=0, column=6)
+
 #:] Music Player
 #:] Daily Time Table
 #:] Pomodoro
