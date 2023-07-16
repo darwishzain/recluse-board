920d40b31fe40a30d4d578654402cb79a229af86
Darwish Mat Zain <darwishzainstudio@gmail.com>
2023-07-17 06:52:13 +0800

    Update Pomodoro Timer
    Variable pomodoro timer

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from pygame import mixer
 import time
 import csv
 import webbrowser
+import calendar
 
 
 #! linux : Tkinter #! win10 : tkinter
@@ -19,28 +20,22 @@ elif os.name == 'nt':
 networks = subprocess.check_output(['netsh', 'wlan', 'show', 'network'])
 decoded_networks = networks.decode('ascii')
 print(decoded_networks) """
-### todo Fix clashing audio
+### todo Fix clashing  pomo+ audio
 ### todo Timetable with choice to show classes only [not important]
-### todo Pomodoro timer -
 
 color1="#4679c6"
 color2="#2f54aa"
 color3="#020a72"
 color4="#073763"
-
-whitesmoke = "#F0F0F0"
-black = "#000000"
-white = "#FFFFFF"
-goldenpoppy = "#F3C300"
-celticblue = "#2E6DB4"
-persiangreen = "#00AC9F"
-cadmiumred = "#DF0024"
+colorb = "#000000"
+colorw = "#FFFFFF"
+colorws = "#F0F0F0"
 
 appName = 'Recluse Board'
 appPlatform = sys.platform
 appOs = os.name
 appDev = 'Darwish Zain'
-appVer = '0.0.1 - reborn'
+appVer = '0.0.2 - baby'
 
 #? run terminal command
 def command(cmd):
@@ -68,27 +63,24 @@ def editor(dir):
 
 root = Tk()
 
-#? frame where everything is in
-#oneFrame = Frame(root).pack()
-#Button(oneFrame, text="what").pack()
-mainFrame = Frame(root, bg=color1, height=300, width=500)
+mainFrame = Frame(root, bg=color1, height=300, width=500)#? frame where everything is in
 mainFrame.grid(row=0, column=0)
 
 viewFrame = Frame(mainFrame, bg=color1)
 viewFrame.grid(row=0,column=0, columnspan=5)
 clockFrame = Frame(mainFrame, bg=color1)
 clockFrame.grid(row=1, column=0,columnspan=5)
-linkFrame = Frame(mainFrame, bg=color2)
+linkFrame = Frame(mainFrame, bg=color1)
 linkFrame.grid(row=2, column=0, columnspan=5)
 shortcutFrame = Frame(mainFrame, bg=color2)
 shortcutFrame.grid(row=3, column=0, columnspan=5)
-audioFrame = Frame(mainFrame, bg=color2, width="500", height="1000")
+audioFrame = Frame(mainFrame, bg=color1, width="500", height="1000")
 audioFrame.grid(row=1, column=5, rowspan=2)
 
 def addButton(line,r,frame,file):
     c = line%5
     if c==0: r += 1
-    Button(frame, text=file[line][0], command=lambda:openurl(file[line][1]),borderwidth=0,bg=color3, fg=whitesmoke, relief="flat", wraplength=80).grid(row=r, column=c,padx=3,pady=3)
+    Button(frame, text=file[line][0], command=lambda:openurl(file[line][1]),borderwidth=0,bg=color3, fg=colorws, relief="flat", wraplength=80).grid(row=r, column=c,padx=3,pady=3)
     return r
 
 
@@ -102,15 +94,25 @@ def initButton(r,file,frame):
         print(str(i)+file[i][0])
         r=addButton(i,r,frame) """
 
-Button(linkFrame, text="Edit Links", command=lambda:editor('/csv/link.csv'),borderwidth=0,bg="#F0F0F0", relief="flat").grid(row=0, column=3,padx=3,pady=3)
-Button(linkFrame, text="Edit Table", command=lambda:editor('/csv/week.csv'),borderwidth=0,bg="#F0F0F0", relief="flat").grid(row=0, column=4,padx=3,pady=3)
+Button(linkFrame, text="Edit Links", command=lambda:editor('/csv/link.csv'),borderwidth=0,bg="#F0F0F0", relief="flat",wraplength=50).grid(row=0, column=3,padx=3,pady=3)
+Button(linkFrame, text="Edit Schedule", command=lambda:editor('/csv/week.csv'),borderwidth=0,bg="#F0F0F0", relief="flat",wraplength=80).grid(row=0, column=4,padx=3,pady=3)
 initButton(1,readcsv('csv/link.csv'),linkFrame)
 initButton(1,readcsv('csv/shortcut.csv'),shortcutFrame)
 ######! START TABLE !######
 def table():
     table=Tk()
+    dayFrame = Frame(table)
+    dayFrame.grid(row=0, column=0)
+    day = ["Day/Time","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    for x in range(25):
+        if(x==0):
+            daytxt = "Day\Time"#column0
+        else:
+            daytxt = "{0:0=2d}".format(x%24)+"00"
+        #Label(dayFrame, justify="center", text=daytxt, width=5,wraplength=80).grid(row=1, column=x, ipady=5, sticky='nesw')
+
     tableFrame = Frame(table)
-    tableFrame.grid(row=0, column=0)
+    tableFrame.grid(row=1, column=0)
     tabledata = readcsv('csv/week.csv')
     for yaxis in range(8):
         for xaxis in range(25):
@@ -125,14 +127,13 @@ def table():
                     tableText = day[yaxis-1]
                 else:
                     tableText = tabledata[yaxis-1][xaxis-1]
-            if xaxis%2==0:
-                bg = color2
-            else:
-                bg = color1
-            Label(tableFrame, justify="center", text=tableText, bg=bg, fg=whitesmoke, wraplength=80).grid(row=yaxis, column=xaxis, ipady=5, sticky='nesw')
+            bg = colorws
+            fg = colorb
+            Label(tableFrame, justify="center", text=tableText, bg=bg, fg=fg, wraplength=100).grid(row=yaxis, column=xaxis, ipady=5, sticky='nesw')
+    table.title("Schedule")
     table.mainloop()
 
-Button(viewFrame, text='Table', borderwidth=0, command=lambda:table()).grid(row=0, column=0)
+Button(viewFrame, text='Schedule', borderwidth=0, command=lambda:table()).grid(row=0, column=0)
 ######! END TABLE !######
 ######! START CLOCK !######
 def initClock():
@@ -143,12 +144,14 @@ def initClock():
     dateDisplay = Label(clockFrame, bg=color1, font=('Arial',20),text="DD/MM/YYYY")
     dateDisplay.grid(row=1, column=0)
 
-    pomo10 = Button(clockFrame, text="+10",borderwidth=0, command=lambda:pomo(10))
-    pomo10.grid(row=1, column=2)
-    pomo30 = Button(clockFrame, text="+30",borderwidth=0, command=lambda:pomo(30))
-    pomo30.grid(row=2, column=2)
-    pomotime = Label(clockFrame, text=timer)
-    pomotime.grid(row=0, column=2)
+    pomotime = Label(clockFrame, text=timer, bg=color1, fg=colorws, font=('Arial',20))
+    pomotime.grid(row=0, column=1)
+
+    pomov = Entry(clockFrame, width=3)
+    pomov.insert(END,"25")
+    pomov.grid(row=0, column=2)
+    pomos = Button(clockFrame, text="Pomodoro", bg=color3, fg=colorws, borderwidth=0, command=lambda:pomo(int(pomov.get())))
+    pomos.grid(row=0, column=3)
     update()
 
 def pomo(t):
@@ -249,8 +252,11 @@ def filenotload():
 audioPlayer()
 initClock()
 
-print(os.listdir("."))
-
+#print(os.listdir("."))
+#year = t("%Y")
+#month = t("%X")
+#print(year+month)
+#print(calendar.month(2023,7))
 root.title(appName+" v"+appVer+" on "+appPlatform)
 #root.geometry("1350x400")
 #root.attributes('-zoomed', True)
