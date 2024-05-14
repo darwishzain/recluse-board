c8b5ed68834c371d9739598bfa87cec634bfde87
darwishzain <darwishzainstudio@gmail.com>
2024-05-15 01:20:45 +0800

    15052024

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import os,sys
+import tkinter as tk
+from tkinter import Label, Frame, Button, filedialog, Entry, Scale
+from tkinter import messagebox
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT']="hide"
 from pygame import mixer
 import time
@@ -7,14 +10,14 @@ import webbrowser
 import calendar
 
 
-#! linux : Tkinter #! win10 : tkinter
-if os.name == 'posix':
-    from Tkinter import *
-    from Tkinter import filedialog
-elif os.name == 'nt':
-    from tkinter import *
-    from tkinter import filedialog
-#from wifi import Cell, Scheme
+#? linux : Tkinter #? win10 : tkinter
+#if os.name == 'posix':
+#    import tkinter as tk
+#    from tkinter import filedialog
+#elif os.name == 'nt':
+#    import tkinter as tk
+#    from tkinter import filedialog
+##from wifi import Cell, Scheme
 
 """ import subprocess
 networks = subprocess.check_output(['netsh', 'wlan', 'show', 'network'])
@@ -31,236 +34,275 @@ colorb = "#000000"
 colorw = "#FFFFFF"
 colorws = "#F0F0F0"
 
-appName = 'Recluse Board'
-appPlatform = sys.platform
-appOs = os.name
-appDev = 'Darwish Zain'
-appVer = '0.0.2 - baby'
+appname = 'Recluse Board'
+appplatform = sys.platform
+appos = os.name
+appdev = 'Darwish Zain'
+appver = '0.0.2 - baby'
 
-#? run terminal command
-def command(cmd):
-    os.system(str(cmd))
-#? open url in browser or file explorer
-def openurl(url):
-    if os.name == 'posix':
-        url ='xdg-open '+url
-    elif os.name == 'nt':
-        url = 'explorer '+url
-    os.system(str(url))
-#? read csv file
-def readcsv(filename):
-    filedata = []
-    if filename:
-        with open(filename,'r') as o:
-            reader = csv.reader(o)
-            for row in reader:
-                filedata.append(row)
-            return filedata
+class RecluseBoard:
+    def __init__(self, root):
+        self.root = root
+        self.root.title(appname+" v"+appver+" on "+appplatform)
+        self.root.geometry("800x600")
+        self.root.attributes('-zoomed', True)
+        self.root.eval('tk::PlaceWindow . center')#? Application positioned at center of screen
 
-#? open editor
-def editor(dir):
-    webbrowser.open(os.getcwd() + dir)
+        #self.root.state('zoomed')
+        #self.root.iconbitmap('image/icon.ico')
+        #print(os.listdir("."))
 
-root = Tk()
+        self.audiolist = []
+        self.audiofile = ""
+        self.init_ui()
+        self.initclock()
+        self.init_audio_player()
+        #self.seeker.set(0)
 
-mainFrame = Frame(root, bg=color1, height=300, width=500)#? frame where everything is in
-mainFrame.grid(row=0, column=0)
+    def openfile(self):
+        self.file_path = filedialog.askopenfilename()
+        if self.file_path:
+            self.welcomelabel.config(text="Selected file: " + os.path.basename(self.file_path))
 
-viewFrame = Frame(mainFrame, bg=color1)
-viewFrame.grid(row=0,column=0, columnspan=5)
-clockFrame = Frame(mainFrame, bg=color1)
-clockFrame.grid(row=1, column=0,columnspan=5)
-linkFrame = Frame(mainFrame, bg=color1)
-linkFrame.grid(row=2, column=0, columnspan=5)
-shortcutFrame = Frame(mainFrame, bg=color2)
-shortcutFrame.grid(row=3, column=0, columnspan=5)
-audioFrame = Frame(mainFrame, bg=color1, width="500", height="1000")
-audioFrame.grid(row=1, column=5, rowspan=2)
+    def editor(self,dir):
+        webbrowser.open(os.getcwd() + dir)
 
-def addButton(line,r,frame,file):
-    c = line%5
-    if c==0: r += 1
-    Button(frame, text=file[line][0], command=lambda:openurl(file[line][1]),borderwidth=0,bg=color3, fg=colorws, relief="flat", wraplength=80).grid(row=r, column=c,padx=3,pady=3)
-    return r
+    def init_ui(self):
+        self.baseframe = Frame(self.root)
+        self.baseframe.pack()
 
+        self.toolbar = Frame(self.baseframe)
+        self.toolbar.pack(anchor="w")
+        self.editlink = Button(self.toolbar, text="Links", command=lambda:self.editor('/csv/link.csv')).grid(row=0, column=0)
+        self.editshortcut = Button(self.toolbar, text="Shortcuts", command=lambda:self.editor('/csv/shortcut.csv')).grid(row=0, column=1)
+        self.editschedule = Button(self.toolbar, text="Schedule", command=lambda:self.editor('/csv/week.csv')).grid(row=0, column=2)
+        #Button(viewFrame, text='Schedule', borderwidth=0, command=lambda:table()).grid(row=0, column=0)
 
-def initButton(r,file,frame):
-    line = len(file)
-    n = 0
-    while(len(file)>n):
-        r = addButton(n,r,frame,file)
-        n+=1
-    """ for i in range (0,len(file)):
-        print(str(i)+file[i][0])
-        r=addButton(i,r,frame) """
+        # Add other UI elements here
+        self.welcomelabel = Label(self.baseframe, text="Welcome to "+appname, font=("Arial", 18))
+        self.welcomelabel.pack(pady=20)
 
-Button(linkFrame, text="Edit Links", command=lambda:editor('/csv/link.csv'),borderwidth=0,bg="#F0F0F0", relief="flat",wraplength=50).grid(row=0, column=3,padx=3,pady=3)
-Button(linkFrame, text="Edit Schedule", command=lambda:editor('/csv/week.csv'),borderwidth=0,bg="#F0F0F0", relief="flat",wraplength=80).grid(row=0, column=4,padx=3,pady=3)
-initButton(1,readcsv('csv/link.csv'),linkFrame)
-initButton(1,readcsv('csv/shortcut.csv'),shortcutFrame)
-######! START TABLE !######
-def table():
-    table=Tk()
-    dayFrame = Frame(table)
-    dayFrame.grid(row=0, column=0)
-    day = ["Day/Time","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-    for x in range(25):
-        if(x==0):
-            daytxt = "Day\Time"#column0
+        self.buttonframe = Frame(self.baseframe)
+        self.buttonframe.pack()
+        #self.button1 = Button(self.baseframe, text="Open File", command=self.openfile)
+        #self.button1.pack()
+        self.linkframe = Frame(self.buttonframe)
+        self.linkframe.grid(row=0,column=0)
+        self.initbutton('csv/link.csv',self.linkframe)
+        self.shortcutframe = Frame(self.buttonframe)
+        self.shortcutframe.grid(row=1,column=0)
+        self.initbutton('csv/shortcut.csv',self.shortcutframe)
+
+    def readcsv(self,filename):
+        filedata = []
+        if filename:
+            with open(filename,'r') as o:
+                reader = csv.reader(o)
+                for row in reader:
+                    filedata.append(row)
+                return filedata
+
+    def command(self,command):
+        os.system(str(command))
+
+    def openurl(self,url):
+        if os.name == 'posix':
+            url ='xdg-open '+url
+        elif os.name == 'nt':
+            url = 'explorer '+url
+        os.system(str(url))
+
+    def addbutton(self,csvline,currentrow,parent,file,maxcolumn):
+        currentcolumn = csvline%maxcolumn
+        if(currentcolumn==0): currentrow+=1
+        Button(parent, text=file[csvline][0], command=lambda:self.openurl(file[csvline][1])).grid(row=currentrow, column=currentcolumn,padx=3,pady=3)
+        return currentrow
+
+    def initbutton(self,dir,parent):
+        csvfile = self.readcsv(dir)
+        #line = len(csvfile)
+        row = 0
+        csvline = 0
+        while(len(csvfile)>csvline):
+            row = self.addbutton(csvline,row,parent,csvfile,5)
+            csvline+=1
+
+#? START audioplayer
+    #TODO FIX prevbtn,nextbtn
+    def init_audio_player(self):
+        self.audioframe = Frame(self.root)
+        self.audioframe.pack()
+
+        self.audiolabel = Label(self.audioframe, text="Audio Title", width="10")
+        self.audiolabel.grid(row=0, column=0, columnspan=5, sticky='nesw')
+
+        #self.seeker = ttk.Scale(self.audioframe, from_=0, to=100, orient=tk.HORIZONTAL)
+        #self.seeker.grid(row=1, column=0,fill=tk.X, padx=10, pady=10)
+        #self.seeker = Scale(self.audioframe, from_=0, to=100, orient=tk.HORIZONTAL,sliderlength=10,length=400)
+        #self.seeker.grid(row=1, column=1,columnspan=5,padx=10, pady=10)
+        #duration = mixer.Sound.get_length()
+        
+        #self.duration = self.sound.get_length()  # Duration of the audio file in seconds
+        #self.channels = self.sound.get_num_channels()  # Number of audio channels (mono or stereo)
+        #self.samplerate = self.sound.get_samplerate()  # Sample rate of the audio file
+
+        self.loadbtn = Button(self.audioframe, text="Load", command=self.load_audio)
+        self.loadbtn.grid(row=2, column=1, columnspan=5)
+
+        self.prevbtn = Button(self.audioframe, text="Prev", command=self.play_audio)
+        self.prevbtn.grid(row=3, column=1)
+        self.playbtn = Button(self.audioframe, text="Play", command=self.play_audio)
+        self.playbtn.grid(row=3, column=2)
+        self.pausebtn = Button(self.audioframe, text="Pause", command=self.pause_audio)
+        self.pausebtn.grid(row=3, column=3)
+        self.stopbtn = Button(self.audioframe, text="Stop", command=self.stop_audio)
+        self.stopbtn.grid(row=3, column=4)
+        self.nextbtn = Button(self.audioframe, text="Next", command=self.play_audio)
+        self.nextbtn.grid(row=3, column=5)
+
+        self.playlistlabel = Label(self.audioframe, wraplength=500)
+        self.playlistlabel.grid(row=3,column=0,columnspan=5)
+        self.isaudioplaying = False
+
+    def load_audio(self):
+        self.audiofile = filedialog.askopenfilename()
+        if self.audiofile:
+            self.audiolabel.config(text=os.path.basename(self.audiofile))
+            self.audiolist.append(self.audiofile)
+
+    def play_audio(self):
+        if self.audiofile:
+            mixer.init()
+            mixer.music.load(self.audiofile)
+            mixer.music.play()
+            self.isaudioplaying = True
+
+    def pause_audio(self):
+        if self.isaudioplaying:
+            mixer.music.pause()
+            self.isaudioplaying = False
         else:
-            daytxt = "{0:0=2d}".format(x%24)+"00"
-        #Label(dayFrame, justify="center", text=daytxt, width=5,wraplength=80).grid(row=1, column=x, ipady=5, sticky='nesw')
+            mixer.music.unpause()
+            self.isaudioplaying = True
 
-    tableFrame = Frame(table)
-    tableFrame.grid(row=1, column=0)
-    tabledata = readcsv('csv/week.csv')
-    for yaxis in range(8):
-        for xaxis in range(25):
-            day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-            if(yaxis==0):
-                if(xaxis==0):
-                    tableText = "Day\Time"#column0
-                else:
-                    tableText = "{0:0=2d}".format(xaxis%24)+"00"
-            else:
-                if(xaxis==0):
-                    tableText = day[yaxis-1]
-                else:
-                    tableText = tabledata[yaxis-1][xaxis-1]
-            bg = colorws
-            fg = colorb
-            Label(tableFrame, justify="center", text=tableText, bg=bg, fg=fg, wraplength=100).grid(row=yaxis, column=xaxis, ipady=5, sticky='nesw')
-    table.title("Schedule")
-    table.mainloop()
+    def stop_audio(self):
+        mixer.music.stop()
+        self.isaudioplaying = False
+#? END audioplayer
+    #def onseek(self,value):
+    #    position = int(value)
+    #    # Update the playback position based on the seeker value
+    #    # Update your audio or video playback position using 'position'
+#? START clock
+    #TODO FIX pomodoro sound
+    def returntime(self,query):
+        return time.strftime(query)
 
-Button(viewFrame, text='Schedule', borderwidth=0, command=lambda:table()).grid(row=0, column=0)
-######! END TABLE !######
-######! START CLOCK !######
-def initClock():
-    global clockDisplay, dateDisplay, timer, pomotime
-    timer = 0
-    clockDisplay = Label(clockFrame, bg=color1, font=('Arial',20), text="HH:MM::SS")
-    clockDisplay.grid(row=0, column=0)
-    dateDisplay = Label(clockFrame, bg=color1, font=('Arial',20),text="DD/MM/YYYY")
-    dateDisplay.grid(row=1, column=0)
+    def returnpomodoro(self, seconds):
+        pomodorominutes = "{:02d}".format(seconds // 60)
+        pomodoroseconds = "{:02d}".format(seconds % 60)
+        return str(pomodorominutes) + ":" + str(pomodoroseconds)
 
-    pomotime = Label(clockFrame, text=timer, bg=color1, fg=colorws, font=('Arial',20))
-    pomotime.grid(row=0, column=1)
+    def setpomo(self,timer):
+        self.pomotimer = timer
 
-    pomov = Entry(clockFrame, width=3)
-    pomov.insert(END,"25")
-    pomov.grid(row=0, column=2)
-    pomos = Button(clockFrame, text="Pomodoro", bg=color3, fg=colorws, borderwidth=0, command=lambda:pomo(int(pomov.get())))
-    pomos.grid(row=0, column=3)
-    update()
+    def initclock(self):
+        #TODO FIX calendar
+        self.clockframe = Frame(self.baseframe)
+        self.clockframe.pack()
 
-def pomo(t):
-    global timer
-    timer = timer + t
+        self.calendarframe = Frame(self.clockframe)
+        self.calendarframe.pack()
 
-def t(q):
-    return time.strftime(q)
+        self.calendarlabel = Label(self.calendarframe, text=calendar.month(int(self.returntime('%Y')),int(self.returntime('%m'))), font=("Arial",12))
+        self.calendarlabel.pack(anchor="w")
+        self.pomotimer = 0
+        self.clocklabel = Label(self.clockframe, text="00:00:00",font=("Arial", 18))
+        self.clocklabel.pack()
 
-def update():
-    global timer
+        self.datelabel = Label(self.clockframe, text="00/00/0000",font=("Arial", 18))
+        self.datelabel.pack()
 
-    clock = t('%H')+":"+t('%M')+":"+t('%S')
-    clockDisplay.config(text=clock)
-    date = t('%d')+"/"+t('%m')+"/"+t('%Y')
-    dateDisplay.config(text=date)
-    pomotime.config(text=timer)
-    if(timer>0):
-        timer -= 1
+        self.pomodoroframe = Frame(self.clockframe)
+        self.pomodoroframe.pack()
+        self.pomodorolabel = Label(self.pomodoroframe, text="00:00",font=("Arial", 18))
+        self.pomodorolabel.grid(row=0,column=0)
+        self.pomodoro30 = Button(self.pomodoroframe, text="+30", command=lambda:self.setpomo(30))
+        self.pomodoro30.grid(row=0, column=1)
+        self.pomodoro60 = Button(self.pomodoroframe, text="+60", command=lambda:self.setpomo(60))
+        self.pomodoro60.grid(row=0, column=2)
+        #self.pomoinput = Entry(self.pomodoroframe, textvariable=30, width=3)
+        #self.pomoinput.grid(row=0, column=3)
+        #self.pomoinputset = Button(self.pomodoroframe, text="Pomo")
+        #self.pomoinputset.grid(row=0, column=4)
+        #self.pomoinput.insert(self.END,"25")
+        self.pomodoro00 = Button(self.pomodoroframe, text="Reset", command=lambda:self.setpomo(0) )
+        self.pomodoro00.grid(row=0, column=5)
 
-    root.after(1000,update)
+        self.updateclock()
 
-######! END CLOCK !######
-#*--------------------*#
-######! START MUSIC PLAYER !######
-#TODO previous, next
-isAudio = False
-audio = mixer
-audio.init()
-audioFile = ""
-audioList = []
-def audioPlayer():
-    global audioLabel,listLabel,listFrame
+    def updateclock(self):
+        clockstr = self.returntime('%H')+":"+self.returntime('%M')+":"+self.returntime('%S')
+        datestr = self.returntime('%d')+"/"+self.returntime('%m')+"/"+self.returntime('%Y')
+        pomodorostr = self.returnpomodoro(self.pomotimer)
+        if(self.pomotimer>0):
+            self.pomotimer-=1
+        #timestr = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+        self.clocklabel.config(text=clockstr)
+        self.datelabel.config(text=datestr)
+        self.pomodorolabel.config(text=pomodorostr)
+        playlist = []
+        if len(self.audiolist) != 0:
+            for audio in range(len(self.audiolist)):
+                playlist.append(str(audio) + " : " + os.path.basename(self.audiolist[int(audio)]))
+            playlisttext = "\n".join(playlist)  # Join the playlist items with newlines
+            self.playlistlabel.config(text=playlisttext)
+        self.root.after(1000, self.updateclock)
 
-    audioLabel = Label(audioFrame, text="Audio Title",width="10")#, relief='flat'
-    audioLabel.grid(row=0, column=0, columnspan=5, sticky='nesw')
-    abFrame = Frame(audioFrame)
-    abFrame.grid(row=1,column=1)
-    playlistFrame = Frame(audioFrame,width=10)
-    playlistFrame.grid(row=4,column=0,columnspan=5)
+#? END clock
 
-    loadBtn = Button(abFrame, text="Load", command=lambda:load())
-    loadBtn.grid(row=1, column=0, columnspan=5)
 
-    prevBtn = Button(abFrame, text="<<", borderwidth=0, state='disabled')
-    prevBtn.grid(row=2, column=0)
-    stopBtn = Button(abFrame, text="Stop", borderwidth=0, command=lambda:stop())
-    stopBtn.grid(row=2, column=1)
-    playBtn = Button(abFrame, text="Play", borderwidth=0, command=lambda:play(len(audioList)-1))
-    playBtn.grid(row=2, column=2)
-    pauseBtn = Button(abFrame, text="Pause", borderwidth=0, command=lambda:pause())
-    pauseBtn.grid(row=2, column=3)
-    nextBtn = Button(abFrame, text=">>", borderwidth=0, state='disabled')
-    nextBtn.grid(row=2, column=4)
+def main():
+    root = tk.Tk()
+    app = RecluseBoard(root)
+    root.mainloop()
 
-    listLabel = Label(playlistFrame, text="Playlist")
-    listLabel.grid(row=1, column=0, columnspan=5)
-    listFrame = Frame(playlistFrame, width=10)
-    listFrame.grid(row=2,column=0, columnspan=5)
+if __name__ == "__main__":
+    main()
 
-def load():
-    global audioFile
-    i = len(audioList)
-    audioFile = filedialog.askopenfilename()
-    if(audioFile):
-        audioLabel.config(text=os.path.basename(audioFile))
-        audioList.append(audioFile)
-        Button(listFrame, text=os.path.basename(audioList[i]),command=lambda:play(i)).grid(row=i, column=0, columnspan=5)
-
-def play(i):
-    audioFile = audioList[i]
-    if audioFile:
-        audio.music.load(audioFile)
-        audio.music.play(loops=-1)
-        isAudio = True
-    else:
-        filenotload()
-
-# *for local variable referenced before assignment,
-# *set variable to global
-# *function asssumes variable is local
-
-def pause():
-    global isAudio#:] solution for referenced before variable
-    if isAudio:
-        mixer.music.unpause()
-    else:
-        mixer.music.pause()
-    isAudio = not isAudio
-
-def stop():
-    mixer.music.stop()
-
-def filenotload():
-    audioLabel.config(text="File Not Loaded")
-
-######! END MUSIC PLAYER !######
-audioPlayer()
-initClock()
-
-#print(os.listdir("."))
-#year = t("%Y")
-#month = t("%X")
-#print(year+month)
-#print(calendar.month(2023,7))
-root.title(appName+" v"+appVer+" on "+appPlatform)
-#root.geometry("1350x400")
-#root.attributes('-zoomed', True)
-#root.state('zoomed')
-root.iconbitmap('image/icon.ico')
-root.eval('tk::PlaceWindow . center')#? Application positioned at center of screen
-root.mainloop()
+#! SCHEDULE to do
+#def table():
+#    table=Tk()
+#    dayFrame = Frame(table)
+#    dayFrame.grid(row=0, column=0)
+#    day = ["Day/Time","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+#    for x in range(25):
+#        if(x==0):
+#            daytxt = "Day\Time"#column0
+#        else:
+#            daytxt = "{0:0=2d}".format(x%24)+"00"
+#        #Label(dayFrame, justify="center", text=daytxt, width=5,wraplength=80).grid(row=1, column=x, ipady=5, sticky='nesw')
+#
+#    tableFrame = Frame(table)
+#    tableFrame.grid(row=1, column=0)
+#    tabledata = readcsv('csv/week.csv')
+#    for yaxis in range(8):
+#        for xaxis in range(25):
+#            day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+#            if(yaxis==0):
+#                if(xaxis==0):
+#                    tableText = "Day\Time"#column0
+#                else:
+#                    tableText = "{0:0=2d}".format(xaxis%24)+"00"
+#            else:
+#                if(xaxis==0):
+#                    tableText = day[yaxis-1]
+#                else:
+#                    tableText = tabledata[yaxis-1][xaxis-1]
+#            bg = colorws
+#            fg = colorb
+#            Label(tableFrame, justify="center", text=tableText, bg=bg, fg=fg, wraplength=100).grid(row=yaxis, column=xaxis, ipady=5, sticky='nesw')
+#    table.title("Schedule")
+#    table.mainloop()
+#
+#######? END TABLE !######
