da25cb618dedaa91b3704994f0616634d42252e3
Darwish Mat Zain <darwishzainstudio@gmail.com>
2024-07-21 20:01:16 +0800

    Update 210720241958
    try to integrate lunowatch [kiv]
    try to fix icon with pillow [kiv]
    fix naming
    clean import
    add and use config

--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
-import os,sys
+import calendar,csv,json,os,requests,sys,time,webbrowser
 import tkinter as tk
-from tkinter import Label, Frame, Button, filedialog, Entry, Scale
-from tkinter import messagebox
+from tkinter import Label,Frame,Button,filedialog,Entry,Scale,messagebox
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT']="hide"
 from pygame import mixer
-import time
-import csv
-import webbrowser
-import calendar
+#from PIL import Image, ImageTk
 
 
 #? linux : Tkinter #? win10 : tkinter
@@ -26,60 +22,67 @@ print(decoded_networks) """
 ### todo Fix clashing  pomo+ audio
 ### todo Timetable with choice to show classes only [not important]
 
-color1="#4679c6"
-color2="#2f54aa"
-color3="#020a72"
-color4="#073763"
+color1 = "#4679c6"
+color2 = "#2f54aa"
+color3 = "#020a72"
+color4 = "#073763"
 colorb = "#000000"
 colorw = "#FFFFFF"
 colorws = "#F0F0F0"
 
-appname = 'Recluse Board'
-appplatform = sys.platform
-appos = os.name
-appdev = 'Darwish Zain'
-appver = '0.0.2 - baby'
+def absolutepath(relative_path):
+        script_dir = os.path.dirname(__file__)
+        return os.path.join(script_dir, relative_path)
 
+def loadconfig(filename=absolutepath('./config.json')):
+    with open(filename, 'r') as file:
+        return json.load(file)
+config = loadconfig()
+
+def editor(dir):
+    #webbrowser.open(os.getcwd() + dir)
+    webbrowser.open(absolutepath(dir))
+
+def command(command):
+    os.system(str(command))
 class RecluseBoard:
     def __init__(self, root):
         self.root = root
-        self.root.title(appname+" v"+appver+" on "+appplatform)
-        self.root.geometry("800x600")
+        self.root.title(f"{config['appname']} v{config['version']}-{config['name']} on {sys.platform}")
+        self.root.geometry(config['windowsize'])
         self.root.attributes('-zoomed', True)
         self.root.eval('tk::PlaceWindow . center')#? Application positioned at center of screen
 
         #self.root.state('zoomed')
-        #self.root.iconbitmap('image/icon.ico')
+        #self.root.iconbitmap(absolutepath('./image/icon.ico'))
         #print(os.listdir("."))
 
         self.audiolist = []
         self.audiofile = ""
-        self.init_ui()
+        self.initui()
         self.initclock()
-        self.init_audio_player()
+        self.initaudioplayer()
         #self.seeker.set(0)
+
 
     def openfile(self):
         self.file_path = filedialog.askopenfilename()
         if self.file_path:
             self.welcomelabel.config(text="Selected file: " + os.path.basename(self.file_path))
 
-    def editor(self,dir):
-        webbrowser.open(os.getcwd() + dir)
-
-    def init_ui(self):
+    def initui(self):
         self.baseframe = Frame(self.root)
         self.baseframe.pack()
 
         self.toolbar = Frame(self.baseframe)
         self.toolbar.pack(anchor="w")
-        self.editlink = Button(self.toolbar, text="Links", command=lambda:self.editor('/csv/link.csv')).grid(row=0, column=0)
-        self.editshortcut = Button(self.toolbar, text="Shortcuts", command=lambda:self.editor('/csv/shortcut.csv')).grid(row=0, column=1)
-        self.editschedule = Button(self.toolbar, text="Schedule", command=lambda:self.editor('/csv/week.csv')).grid(row=0, column=2)
+        self.editlink = Button(self.toolbar, text="Links", command=lambda:editor(config['csvfile']['link'])).grid(row=0, column=0)
+        self.editshortcut = Button(self.toolbar, text="Shortcuts", command=lambda:editor(config['csvfile']['shortcut'])).grid(row=0, column=1)
+        self.editschedule = Button(self.toolbar, text="Schedule", command=lambda:editor(config['csvfile']['week'])).grid(row=0, column=2)
         #Button(viewFrame, text='Schedule', borderwidth=0, command=lambda:table()).grid(row=0, column=0)
 
         # Add other UI elements here
-        self.welcomelabel = Label(self.baseframe, text="Welcome to "+appname, font=("Arial", 18))
+        self.welcomelabel = Label(self.baseframe, text="Welcome to "+config['appname'], font=("Arial", 18))
         self.welcomelabel.pack(pady=20)
 
         self.buttonframe = Frame(self.baseframe)
@@ -88,22 +91,20 @@ class RecluseBoard:
         #self.button1.pack()
         self.linkframe = Frame(self.buttonframe)
         self.linkframe.grid(row=0,column=0)
-        self.initbutton('csv/link.csv',self.linkframe)
+        self.initbutton('./csv/link.csv',self.linkframe)
         self.shortcutframe = Frame(self.buttonframe)
         self.shortcutframe.grid(row=1,column=0)
-        self.initbutton('csv/shortcut.csv',self.shortcutframe)
+        self.initbutton('./csv/shortcut.csv',self.shortcutframe)
 
     def readcsv(self,filename):
         filedata = []
         if filename:
-            with open(filename,'r') as o:
+            absolutefilepath = absolutepath(filename)
+            with open(absolutefilepath,'r') as o:
                 reader = csv.reader(o)
                 for row in reader:
                     filedata.append(row)
                 return filedata
-
-    def command(self,command):
-        os.system(str(command))
 
     def openurl(self,url):
         if os.name == 'posix':
@@ -119,7 +120,7 @@ class RecluseBoard:
         return currentrow
 
     def initbutton(self,dir,parent):
-        csvfile = self.readcsv(dir)
+        csvfile = self.readcsv(absolutepath(dir))
         #line = len(csvfile)
         row = 0
         csvline = 0
@@ -129,55 +130,55 @@ class RecluseBoard:
 
 #? START audioplayer
     #TODO FIX prevbtn,nextbtn
-    def init_audio_player(self):
+    def initaudioplayer(self):
         self.audioframe = Frame(self.root)
         self.audioframe.pack()
 
-        self.audiolabel = Label(self.audioframe, text="Audio Title", width="10")
-        self.audiolabel.grid(row=0, column=0, columnspan=5, sticky='nesw')
+        self.audiolabel = Label(self.audioframe, text="Audio Title")
+        self.audiolabel.grid(row=0, column=0, columnspan=5)
 
         #self.seeker = ttk.Scale(self.audioframe, from_=0, to=100, orient=tk.HORIZONTAL)
         #self.seeker.grid(row=1, column=0,fill=tk.X, padx=10, pady=10)
         #self.seeker = Scale(self.audioframe, from_=0, to=100, orient=tk.HORIZONTAL,sliderlength=10,length=400)
         #self.seeker.grid(row=1, column=1,columnspan=5,padx=10, pady=10)
         #duration = mixer.Sound.get_length()
-        
+
         #self.duration = self.sound.get_length()  # Duration of the audio file in seconds
         #self.channels = self.sound.get_num_channels()  # Number of audio channels (mono or stereo)
         #self.samplerate = self.sound.get_samplerate()  # Sample rate of the audio file
 
-        self.loadbtn = Button(self.audioframe, text="Load", command=self.load_audio)
+        self.loadbtn = Button(self.audioframe, text="Load", command=self.loadaudio)
         self.loadbtn.grid(row=2, column=1, columnspan=5)
 
-        self.prevbtn = Button(self.audioframe, text="Prev", command=self.play_audio)
+        self.prevbtn = Button(self.audioframe, text="Prev", command=self.playaudio)
         self.prevbtn.grid(row=3, column=1)
-        self.playbtn = Button(self.audioframe, text="Play", command=self.play_audio)
+        self.playbtn = Button(self.audioframe, text="Play", command=self.playaudio)
         self.playbtn.grid(row=3, column=2)
-        self.pausebtn = Button(self.audioframe, text="Pause", command=self.pause_audio)
+        self.pausebtn = Button(self.audioframe, text="Pause", command=self.pauseaudio)
         self.pausebtn.grid(row=3, column=3)
-        self.stopbtn = Button(self.audioframe, text="Stop", command=self.stop_audio)
+        self.stopbtn = Button(self.audioframe, text="Stop", command=self.stopaudio)
         self.stopbtn.grid(row=3, column=4)
-        self.nextbtn = Button(self.audioframe, text="Next", command=self.play_audio)
+        self.nextbtn = Button(self.audioframe, text="Next", command=self.playaudio)
         self.nextbtn.grid(row=3, column=5)
 
         self.playlistlabel = Label(self.audioframe, wraplength=500)
-        self.playlistlabel.grid(row=3,column=0,columnspan=5)
+        self.playlistlabel.grid(row=4,column=0, columnspan=5)
         self.isaudioplaying = False
 
-    def load_audio(self):
+    def loadaudio(self):
         self.audiofile = filedialog.askopenfilename()
         if self.audiofile:
             self.audiolabel.config(text=os.path.basename(self.audiofile))
             self.audiolist.append(self.audiofile)
 
-    def play_audio(self):
+    def playaudio(self):
         if self.audiofile:
             mixer.init()
             mixer.music.load(self.audiofile)
             mixer.music.play()
             self.isaudioplaying = True
 
-    def pause_audio(self):
+    def pauseaudio(self):
         if self.isaudioplaying:
             mixer.music.pause()
             self.isaudioplaying = False
@@ -185,7 +186,7 @@ class RecluseBoard:
             mixer.music.unpause()
             self.isaudioplaying = True
 
-    def stop_audio(self):
+    def stopaudio(self):
         mixer.music.stop()
         self.isaudioplaying = False
 #? END audioplayer
@@ -306,3 +307,55 @@ if __name__ == "__main__":
 #    table.mainloop()
 #
 #######? END TABLE !######
+
+#! icon with pillow
+#        iconpath = self.geticonpath('./image/icon.png')
+#        self.seticon(iconpath)
+#
+#    def geticonpath(self, filename):
+#        """ Returns the absolute path to the icon file. """
+#        if getattr(sys, 'frozen', False):
+#            # If running in a frozen state (e.g., PyInstaller), use the bundled path
+#            return os.path.join(sys._MEIPASS, filename)
+#        else:
+#            # Running in a development environment
+#            return os.path.join(os.path.dirname(__file__), filename)
+#
+#    def seticon(self, iconpath):
+#        """ Sets the window icon using Pillow. """
+#        try:
+#            icon = Image.open(iconpath)
+#            self.root.iconphoto(True, ImageTk.PhotoImage(icon))
+#        except Exception as e:
+#            print(f"Error setting icon: {e}")
+
+#! Luno price tracker
+#        self.getlunoprice()
+#        self.tracklunoprice()
+#
+#        self.lunourl = 'https://api.luno.com/api/1/ticker?pair=XRPMYR'
+#
+#    def getlunoprice(self):
+#        try:
+#            response = requests.get(self.lunorul)
+#            data = response.json()
+#            if 'last_trade' in data:
+#                price = data['last_trade']
+#                return price
+#            else:
+#                return None
+#        except Exception as e:
+#            print(f"Error fetching Luno price: {e}")
+#            return None
+#
+#    def tracklunoprice(self,intervalseconds):
+#        while True:
+#            price = self.getlunoprice()
+#            if price is not None:
+#                print(f"Luno XRP/MYR Price: {price}")
+#            else:
+#                print("Unable to fetch Luno price.")
+#            time.sleep(self.intervalseconds)
+#
+#    # Track Luno price every 60 seconds
+#    self.tracklunoprice(self,60)
