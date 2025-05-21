a46eb85e8cdfbf44a8653a0b987f634777c74eaa
Darwish Mat Zain <darwishzainstudio@gmail.com>
2025-05-21 12:07:33 +0800

    Add media load function
    using one button for play and pause not working. need to rework the condition

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import calendar
+from datetime import timedelta
 import json
 import os
 import requests
 import sys
 import time
 import webbrowser
-from PyQt6.QtWidgets import QApplication, QGridLayout, QLabel, QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtCore import QTimer
+from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import QApplication, QFileDialog, QGridLayout, QLabel, QMainWindow, QPushButton, QVBoxLayout, QWidget
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame import mixer
 
@@ -32,16 +35,36 @@ class RecluseWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle(config['appname'])
         self.setGeometry(100, 100, int(config['height']), int(config['width']))  # x, y, height, width
+        self.fontsmall = QFont(config['fontfamily'], int(config['fontsize']['small']))
+        self.fontmedium = QFont(config['fontfamily'], int(config['fontsize']['medium']))
+        self.fontlarge = QFont(config['fontfamily'], int(config['fontsize']['large']))
 
         self.container = QWidget(self)
+        self.container.setFont(self.fontsmall)
         self.setCentralWidget(self.container)
+
         self.grid = QGridLayout()  # row, column, span row, span column
         self.container.setLayout(self.grid)
         self.container.setStyleSheet('background-color:' + config['background'] + ';color:' + config['foreground'])
 
         self.init_ui()
+        #* Buttons(Links,Projects,Commands,etc)
         self.buttons()
+        #*Media Player
         self.mediaplayer()
+        self.mixer = mixer
+        self.mixer.init()
+        self.isplaying = False
+        self.ispaused = True
+        self.mediafile = None
+        #* Clock, Date, Pomodoro
+        self.clock()
+
+        #* Update(1000ms)
+        self.update()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(1000)
 
     def init_ui(self):
         self.welcome = QLabel("Welcome to Recluse Board", self.container)
@@ -51,11 +74,15 @@ class RecluseWindow(QMainWindow):
         # Set up button widget and media widget
         self.buttonwidget = QWidget(self.container)
         self.buttonlayout = QGridLayout(self.buttonwidget)  # Set layout for button widget
-        self.grid.addWidget(self.buttonwidget, 1, 0)
+        self.grid.addWidget(self.buttonwidget, 2, 0)
 
         self.mediawidget = QWidget(self.container)
-        self.grid.addWidget(self.mediawidget, 1, 1)
+        self.grid.addWidget(self.mediawidget, 2, 1)
         self.medialayout = QVBoxLayout(self.mediawidget)
+
+        self.timewidget = QWidget(self.container)
+        self.timelayout = QGridLayout(self.timewidget)
+        self.grid.addWidget(self.timewidget,1,0)
 
     def buttons(self):
         counter = 0
@@ -88,22 +115,48 @@ class RecluseWindow(QMainWindow):
         self.medialayout.addWidget(self.nextbtn)
 
         # Connect media buttons to methods
-        self.loadbtn.clicked.connect(self.load_media)
-        self.prevbtn.clicked.connect(self.prev_media)
-        self.playbtn.clicked.connect(self.play_media)
-        self.nextbtn.clicked.connect(self.next_media)
+        self.loadbtn.clicked.connect(self.loadmedia)
+        self.prevbtn.clicked.connect(self.prevmedia)
+        self.playbtn.clicked.connect(self.playmedia)
+        self.nextbtn.clicked.connect(self.nextmedia)
 
-    def load_media(self):
-        print("Load media")
+    def loadmedia(self):
+        file = QFileDialog.getOpenFileName(self, 'Open File', '', 'Audio Files (*.mp3 *.wav *.ogg)')[0]
+        if file:
+            self.mediafile = file
+            self.mixer.music.load(self.mediafile)
+            self.medialabel.setText(os.path.basename(file))
 
-    def prev_media(self):
+    def mediaplaylist(self):
+        print(self.mediaplaylist)
+    def prevmedia(self):
         print("Previous media")
 
-    def play_media(self):
-        print("Play media")
+    def playmedia(self):
+        if self.mediafile:
+            print("Playing media")
 
-    def next_media(self):
+    def nextmedia(self):
         print("Next media")
+
+    def clock(self):
+        self.pomotime = 0
+        self.clock = QLabel("HH:MM:SS")
+        self.timelayout.addWidget(self.clock)
+        self.date = QLabel("DD/MM/YY")
+        self.timelayout.addWidget(self.date)
+        self.pomo = QLabel("HH:MM:SS")
+        self.timelayout.addWidget(self.pomo)
+
+    def update(self):
+        clockstr = time.strftime('%H')+":"+time.strftime('%M')+":"+time.strftime('%S')
+        datestr = time.strftime('%d')+"/"+time.strftime('%m')+"/"+time.strftime('%Y')
+        if self.pomotime > 0:
+            self.pomotime = self.pomotime - 1
+        self.clock.setText(clockstr)
+        self.date.setText(datestr)
+        self.pomo.setText(str(timedelta(seconds=self.pomotime)))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
