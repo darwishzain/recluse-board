5185a4d05880d88fc23f21acc775182749174187
Darwish Mat Zain - Hidari Shoya <darwishzainstudio@gmail.com>
2025-09-24 03:13:10 +0800

    update and fix
    - add requirements.txt
    - fix the appearance
    - fix default config.json and data.json
    - update .gitignore for venvs

--- src/main.py
+++ src/main.py
@@ -4,27 +4,26 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame import mixer
 from PyQt6.QtCore import Qt, QTimer, QSize
 from PyQt6.QtGui import QFont, QIcon
-from PyQt6.QtWidgets import QApplication, QFileDialog, QGridLayout, QHBoxLayout, QLabel, QListWidget, QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QFileDialog, QGridLayout, QHBoxLayout, QLabel, QLineEdit,QListWidget,QListWidgetItem, QMainWindow, QPushButton, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
 
-print(socket.gethostbyname(socket.gethostname()))
+#print(socket.gethostbyname(socket.gethostname()))
 
 
 def command(command):
     os.system(str(command))
 
 class RecluseWindow(QMainWindow):
-    def fullpath(self,relativepath):
-        directory = os.path.dirname(__file__)
+    def fullpath(self,relativepath:str)->str:
+        directory = os.path.dirname(os.path.abspath(__file__))
         return(os.path.join(directory,relativepath))
 
     def openjson(self,filename):
         file = self.fullpath(filename)
         with open(file) as f:
-            jsondata = json.load(f)
-        return(jsondata)
+            return(json.load(f))
 
-    def editor(self,filedirectory):
-        webbrowser.open(self.fullpath(filedirectory))
+    def editor(self,path):
+        webbrowser.open(self.fullpath(path))
 
     def __init__(self):
         super().__init__()
@@ -32,28 +31,40 @@ class RecluseWindow(QMainWindow):
         self.setWindowTitle(self.config['appname'])
         self.setWindowIcon(QIcon(self.fullpath("graphic/icon.ico")))
         self.setGeometry(100, 100, int(self.config['height']), int(self.config['width']))  # x, y, height, width
-        self.fontsmall = QFont(self.config['fontfamily'], int(self.config['fontsize']['small']))
-        self.fontmedium = QFont(self.config['fontfamily'], int(self.config['fontsize']['medium']))
-        self.fontlarge = QFont(self.config['fontfamily'], int(self.config['fontsize']['large']))
-
+        font = self.config['fontfamily']
+        fontsize = self.config['fontsize']
+        self.fontsmall = QFont(font, int(fontsize['small']))
+        self.fontmedium = QFont(font, int(fontsize['medium']))
+        self.fontlarge = QFont(font, int(fontsize['large']))
+        self.light = self.config['light']
+        self.dark = self.config['dark']
+        self.tomato = self.config['tomato']
         self.container = QWidget(self)
-        self.container.setFont(self.fontsmall)
         self.setCentralWidget(self.container)
+        self.container.setFont(self.fontsmall)
 
-        self.self = QGridLayout()  # row, column, span row, span column
-        self.container.setLayout(self.self)
-        self.container.setStyleSheet('background-color:' + self.config['background'] + ';color:' + self.config['foreground'])
-        #initui
-        welcome = QLabel("Welcome to Recluse Board", self.container)
-        welcome.setGeometry(0, 0, 300, 400)
-        self.self.addWidget(welcome, 0, 0, 1, 3)
+        self.layout = QVBoxLayout()
+        self.container.setLayout(self.layout)
 
+
+        #self.self = QGridLayout()  # row, column, span row, span column
+        #self.container.setLayout(self.self)
+        #self.container.setStyleSheet('background-color:' + self.config['background'] + ';color:' + self.config['foreground'])
+        ##initui
+        #welcome = QLabel("Welcome to Recluse Board", self.container)
+        #welcome.setGeometry(0, 0, 300, 400)
+        #self.self.addWidget(welcome, 0, 0, 1, 3)
+
+        #* Clock, Date, Pomodoro
+        self.time()
+
+        self.middle = QHBoxLayout()
+        self.layout.addLayout(self.middle)
         #* Buttons(Links,Projects,Commands,etc)
         self.buttons()
         #*Media Player
         self.mediaplayer()
-        #* Clock, Date, Pomodoro
-        self.clock()
+
 
         #* Update(1000ms)
         self.update()
@@ -62,21 +73,35 @@ class RecluseWindow(QMainWindow):
         self.timer.start(1000)
 
     def buttons(self):
-        buttonwidget = QWidget(self.container)
-        buttonlayout = QGridLayout(buttonwidget)  # Set layout for button widget
-        self.self.addWidget(buttonwidget, 2, 0)
         counter = 0
-        # Assuming openjson('./data.json')['link'] returns a dictionary of text and url
-        for text, url in self.openjson('./data.json')['link'].items():
-            row = counter // 5
-            column = counter % 5
-            button = QPushButton(text, buttonwidget)
-            button.clicked.connect(lambda checked, url=url: self.open_url(url))  # Connect button to URL
-            buttonlayout.addWidget(button, row, column)
-            counter += 1
+        buttonwidget = QWidget()
+        buttonlayout = QGridLayout(buttonwidget)
+        self.middle.addWidget(buttonwidget)
+        self.data = self.openjson('data.json')
+        links = self.data.get('link', {})
+        commands = self.data.get('command', {})
+        if links:
+            for text,url in links.items():
+                row = counter // 5
+                column = counter % 5
+                button =QPushButton(text,buttonwidget)
+                button.clicked.connect(lambda checked,url=url:self.openurl(url))
+                buttonlayout.addWidget(button,row,column)
+                counter += 1
+        if commands:
+            for text,cmd in commands.items():
+                row = counter // 5
+                column = counter % 5
+                button =QPushButton(text,buttonwidget)
+                button.clicked.connect(lambda checked,cmd=cmd:self.runcommand(cmd))
+                buttonlayout.addWidget(button,row,column)
+                counter += 1
 
-    def open_url(self, url):
+    def openurl(self, url):
         webbrowser.open(url)  # Open the URL in the browser
+
+    def runcommand(self,cmd):
+        os.system(str(cmd))
 
     def mediaplayer(self):
         self.mixer = mixer
@@ -84,21 +109,26 @@ class RecluseWindow(QMainWindow):
         self.isplaying = False
         self.ispaused = True
         self.mediafile = None
-        mediawidget = QWidget(self.container)
-        self.self.addWidget(mediawidget, 1, 1)
-        
-        mediatitle = QVBoxLayout(mediawidget)
-        self.medialabel = QLabel("Media Player")
-        self.medialabel.setFixedWidth(200)
-        mediatitle.addWidget(self.medialabel, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.playindex = 0
+        self.playlist = []
+        mediawidget = QWidget()
+        medialayout = QVBoxLayout(mediawidget)
+        self.middle.addWidget(mediawidget)
+
+        medialayout.addWidget(QLabel("Media Player"))
+        self.mediatitle = QLabel("Player Empty")
+        medialayout.addWidget(self.mediatitle)
 
         mediacontrol = QHBoxLayout()
-        mediatitle.addLayout(mediacontrol)
-
+        medialayout.addLayout(mediacontrol)
+        #mediatitle = QVBoxLayout(mediawidget)
+        #self.medialabel = QLabel("Media Player")
+        #self.medialabel.setFixedWidth(200)
+        #mediatitle.addWidget(self.medialabel, alignment=Qt.AlignmentFlag.AlignCenter)
         loadbtn = QPushButton()
         loadbtn.setIcon(QIcon(self.fullpath('graphic/load.png')))
         mediacontrol.addWidget(loadbtn)
-
+#
         prevbtn = QPushButton()
         prevbtn.setIcon(QIcon(self.fullpath('graphic/previous.png')))
         mediacontrol.addWidget(prevbtn)
@@ -118,63 +148,90 @@ class RecluseWindow(QMainWindow):
         # Connect media buttons to methods
         loadbtn.clicked.connect(self.loadmedia)
         prevbtn.clicked.connect(self.prevmedia)
-        self.playbtn.clicked.connect(self.playmedia)
+        self.playbtn.clicked.connect(self.play)
         nextbtn.clicked.connect(self.nextmedia)
-        
-        self.playlist = QListWidget()
-        mediatitle.addWidget(self.playlist)
+
+        self.mediaplaylist = QListWidget()
+        medialayout.addWidget(self.mediaplaylist)
 
     def loadmedia(self):
         file = QFileDialog.getOpenFileName(self, 'Open File', '', 'Audio Files (*.mp3 *.wav *.ogg)')[0]
         if file:
             self.mediafile = file
-            self.playlist.addItem(file)
-#*.m4a *.flac *.aac *.opus *.aiff *.wma
-    def mediaplaylist(self):
-        print(self.mediaplaylist)
+            self.playlist.append({"title": os.path.basename(file), "path": file})
+            self.mediaplaylist.clear()
+            for index, media in enumerate(self.playlist):
+                item = QListWidgetItem(media['title'])
+                btn = QPushButton(media['title'])
+                btn.clicked.connect(lambda checked, index=index: self.play(index))
+                self.mediaplaylist.addItem(item)
+                self.mediaplaylist.setItemWidget(item, btn)
 
-    def playmedia(self):
-        item = self.playlist.currentItem()
-        if item is None and self.playlist.count() > 0:
-            item = self.playlist.item(0)
-            self.playlist.setCurrentItem(item)
-        if item:
-            path = item.text()
-            self.medialabel.setText(os.path.basename(path))
-            self.medialabel.setToolTip(os.path.basename(path))
-            self.mixer.music.load(path)
-            self.mixer.music.play()
-    
+    def play(self,index):
+        if index is None:
+            if not self.playlist:
+                return
+            else:
+                media = self.playlist[0]
+                self.playindex = 0
+        else:
+            media = self.playlist[index]
+            self.playindex = index
+        print(media['path'])
+        self.mediatitle.setText(media['title'])
+        self.mediatitle.setToolTip(media['title'])
+        self.mixer.music.load(media['path'])
+        self.mixer.music.play()
+
     def prevmedia(self):
-        count = self.playlist.count()
-        if count == 0:
-            return
-        current_row = self.playlist.currentRow()
-        prev_row = (current_row - 1 + count) % count  # 🔁 loop back to last
-        self.playlist.setCurrentRow(prev_row)
-        self.playmedia()
+        self.play(self.playindex-1)
 
     def nextmedia(self):
-        count = self.playlist.count()
-        if count == 0:
-            return
-        current_row = self.playlist.currentRow()
-        next_row = (current_row + 1) % count  # 🔁 loop back to 0
-        self.playlist.setCurrentRow(next_row)
-        self.playmedia()
+        self.play(self.playindex+1)
 
+    def pomodoro(self,time):
+        self.pomotime = time
 
-    def clock(self):
-        timewidget = QWidget(self.container)
+    def time(self):
+        self.pomotime = 30
+        timewidget = QWidget()
         timelayout = QHBoxLayout(timewidget)
-        self.self.addWidget(timewidget,1,0)
-        self.pomotime = 0
+        self.layout.addWidget(timewidget)
+        timewidget.setStyleSheet(self.tomato)
+
+        clocklayout = QHBoxLayout()
+        timelayout.addLayout(clocklayout)
         self.clock = QLabel("HH:MM:SS")
-        timelayout.addWidget(self.clock)
-        self.date = QLabel("DD/MM/YY")
-        timelayout.addWidget(self.date)
-        self.pomo = QLabel("HH:MM:SS")
-        timelayout.addWidget(self.pomo)
+        self.clock.setFont(self.fontlarge)
+        clocklayout.addWidget(self.clock)
+        self.date = QLabel("DD/MM/YYYY")
+        self.date.setFont(self.fontlarge)
+        clocklayout.addWidget(self.date)
+
+        pomolayout = QVBoxLayout()
+        timelayout.addLayout(pomolayout)
+
+        self.pomotimer = QLabel("HH:MM:SS")
+        self.pomotimer.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        pomolayout.addWidget(self.pomotimer,stretch=1)
+        self.pomotimer.setFont(self.fontlarge)
+        pomocontrol = QHBoxLayout()
+        pomolayout.addLayout(pomocontrol)
+        self.pomo0 = QPushButton("Refresh")
+        pomocontrol.addWidget(self.pomo0,stretch=0)
+        self.pomo0.clicked.connect(lambda checked,time=0:self.pomodoro(time))
+        self.pomo30 = QPushButton("+30")
+        self.pomo30.clicked.connect(lambda checked,time=30:self.pomodoro(time))
+        pomocontrol.addWidget(self.pomo30,stretch=0)
+
+        pomoinput = QHBoxLayout()
+        pomolayout.addLayout(pomoinput)
+        self.pomoinput = QLineEdit()
+        pomocontrol.addWidget(self.pomoinput,stretch=0)
+        self.pomoinput.setStyleSheet('background-color:#F0F0F0;color:#000000;')
+        self.pomocustom = QPushButton("+")
+        pomocontrol.addWidget(self.pomocustom,stretch=0)
+
 
     def update(self):
         clockstr = time.strftime('%H')+":"+time.strftime('%M')+":"+time.strftime('%S')
@@ -183,11 +240,31 @@ class RecluseWindow(QMainWindow):
             self.pomotime = self.pomotime - 1
         self.clock.setText(clockstr)
         self.date.setText(datestr)
-        self.pomo.setText(str(timedelta(seconds=self.pomotime)))
+        self.pomotimer.setText(str(timedelta(seconds=self.pomotime)))
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setStyleSheet("""
+        QPushButton {
+            background-color: #F0F0F0;
+            color: #000000;
+            border-radius: 6px;
+            padding: 6px 12px;
+        }
+        QPushButton:hover {
+            background-color: #2E6DB4;
+            color:#FFFFFF;
+        }
+        QPushButton:pressed {
+            background-color: #E5533D;
+        }
+        QLineEdit{
+            background-color:#F0F0F0;
+            color:#FFFFFF;
+        }
+    """)
+
     window = RecluseWindow()
     window.show()
     sys.exit(app.exec())
